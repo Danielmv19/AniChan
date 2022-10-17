5a5d8f44d3a3e5025eb3db9fbf4adfd554503fc1
Jose Mercado <josuemercado32@gmail.com>
2022-10-17 10:06:20 -0500

    cita random + filtro por anime + filtro por personaje

--- AnimeChan.py
+++ AnimeChan.py
@@ -1,5 +1,6 @@
-import requests
+import random
 
+import requests
 def a():
     url = 'https://animechan.vercel.app/api/random'
     r = requests.get(url)
@@ -8,20 +9,43 @@ def a():
     anime = j['anime']
     character = j['character']
     quote = j['quote']
+    print(f'Anime: {anime}\nPersonaje: {character}\nCita: {quote}')
     #------- _** = negrita en discord :v -------------
-    print(f'_**Anime :**_ {anime}\n _**Personaje :_** {character}\n _**Cita :_** {quote}')
+    #print(f'_**Anime :**_ {anime}\n _**Personaje :_** {character}\n _**Cita :_** {quote}')
 a()
 
-print('\n')
-
+print('----------------------------------------------------------------')
 def b():
-    url = 'https://animechan.vercel.app/api/quotes/anime?title=naruto'
+    animeName = input("Ingrese el nombre del anime: ")
+    url = f'https://animechan.vercel.app/api/quotes/anime?title={animeName}'
     r = requests.get(url)
     j = r.json()
     total =(len(j))
-    one = j[0]
+    totalRandom = random.choice(range(0, total))
+    print(f'cita {totalRandom} de {total}')
+    one = j[totalRandom]
+    anime = one['anime']
+    character = one['character']
+    quote = one['quote']
 
-    print('anime: ', one['anime'])
-    print('personaje: ', one['character'])
-    print('cita: ', one['quote'])
+    print(f'Anime: {anime}\nPersonaje: {character}\nCita: {quote}')
+
 b()
+print('----------------------------------------------------------------')
+def c():
+    characName = input("Ingrese el nombre del personaje: ")
+    url = f'https://animechan.vercel.app/api/quotes/character?name={characName}'
+    r = requests.get(url)
+    j = r.json()
+    total =(len(j))
+    totalRandom = random.choice(range(0, total))
+    print(f'cita {totalRandom} de {total}')
+
+    one = j[totalRandom]
+    anime = one['anime']
+    character = one['character']
+    quote = one['quote']
+
+    print(f'Anime: {anime}\nPersonaje: {character}\nCita: {quote}')
+
+c()
