f79fde0eb9a859472509dd632679a552b21352e4
Jose Mercado <josuemercado32@gmail.com>
2022-10-17 00:03:21 -0500

    agragdo cita random

--- AnimeChan.py
+++ AnimeChan.py
@@ -5,22 +5,23 @@ def a():
     r = requests.get(url)
     j = r.json()
 
+    anime = j['anime']
+    character = j['character']
+    quote = j['quote']
+    #------- _** = negrita en discord :v -------------
+    print(f'_**Anime :**_ {anime}\n _**Personaje :_** {character}\n _**Cita :_** {quote}')
+a()
 
-    #print(f'_**Anime :**_ {anime}\n _**Personaje :_** {character}\n _**Cita :_** {quote}')
+print('\n')
 
 def b():
     url = 'https://animechan.vercel.app/api/quotes/anime?title=naruto'
     r = requests.get(url)
     j = r.json()
     total =(len(j))
-    on = j[0]
-    print(on['anime'])
-    print(on['character'])
-    print(on['quote'])
-    print(on)
-    print(type(j))
-    #character = j["character"]
-    #quote = j["quote"]
-    #Personaje : {character}\n Cita : {quote}
+    one = j[0]
 
+    print('anime: ', one['anime'])
+    print('personaje: ', one['character'])
+    print('cita: ', one['quote'])
 b()
